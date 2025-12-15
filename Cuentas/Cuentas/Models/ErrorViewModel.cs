187ca7fb90db9d8c0b0a12b618a279ac85eb6550
DelverEspinal <delvervarela66@gmail.com>
2025-12-15 16:32:39 -0600

    si

--- Cuentas/Cuentas/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Cuentas.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
